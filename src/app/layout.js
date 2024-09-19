a190e1f8bcfa1a45127b5558abf3ad83041d1865
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-19 02:27:51 +0100

    folder structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={inter.className}
+      >{children}
       </body>
     </html>
   );
